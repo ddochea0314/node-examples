ee4a40085f5a85a4dccec532c4a8e84c5f675760
ddochea <ddochea0314@gmail.com>
2021-03-01 23:28:16 +0900

    packageDefinition param 설명추가

--- grpc/1_helloworld_client.js
+++ grpc/1_helloworld_client.js
@@ -4,11 +4,12 @@ const protoLoader = require('@grpc/proto-loader');
 var packageDefinition = protoLoader.loadSync(
     __dirname + '/protos/helloworld.proto',
     {
-        keepCase: true,
-        longs: String,
-        enums: String,
-        defaults: true,
-        oneofs: true
+        // https://github.com/grpc/grpc-node/blob/master/packages/proto-loader/README.md#usage
+        keepCase: true, // 필드이름을 유지여부. 기본값은 camelCase로 변경합니다. (Preserve field names. The default is to change them to camel case.)
+        longs: String, // 긴값을 나타내는 유형. 기본값은 'Number'입니다. The type to use to represent long values. Defaults to a Long object type.
+        enums: String, // enum 을 나타내는 유형.
+        defaults: true, // 출력 개체 기본값 설정여부 
+        oneofs: true // 가상속성을 현재 필드 이름으로 설정(?)
     }
 )
 
