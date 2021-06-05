a127f7758e61d8efe10063b1cbd62f8d444f059f
ddochea0314 <ddochea0314@gmail.com>
2021-06-05 11:13:27 +0900

    성능확인용 코드 추가

--- sequelize-helloworld/index.ts
+++ sequelize-helloworld/index.ts
@@ -1,18 +1,23 @@
 import sequelize from "./models/index";
 import { initModels, Users } from "./models/init-models";
-
 async function main() {
     initModels(sequelize);
-    const users = await Users.findAll(); // select
-    users.forEach(u => {
-        console.log(u.email);
-    })
-    Users.upsert({
-        id : 1,
-        firstName : "B",
-        lastName: "hello",
-        email : "test@test.com"
-    }) // insert & update
+    // const users = await Users.findAll(); // select
+    // users.forEach(u => {
+    //     console.log(u.email);
+    // })
+    for (let a = 0; a < 5; a++) {
+        console.time("stopwatch");
+        for (let i = 1; i < 1001; i++) {
+            await Users.upsert({
+                id : i,
+                firstName : "B",
+                lastName: "hello",
+                email : "test@test.com"
+            }) // insert & update
+        }
+        console.timeEnd("stopwatch");
+    }
 }
 
 main();
